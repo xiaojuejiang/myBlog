40e362123dfdf8933958f934e4e4a009fe9eda5b
xiaojuejiang <370208645@qq.com>
2018-11-05 20:51:12 +0800

    完成了导航与登录注册功能

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express=require('express')
 
 const app=express()
 
+const indexRouter=require('./router/index.js')
+const userRouter=require('./router/user.js')
+
+
+app.use(indexRouter)
+
+app.use(userRouter)
+
 app.use(express.static('./views'))
 
 app.use('/node_modules',express.static('./node_modules'))
@@ -9,10 +17,6 @@ app.use('/node_modules',express.static('./node_modules'))
 app.set('view engine','ejs')
 
 app.set('views','./views')
-
-app.get('/',(req,res)=>{
-    res.render('index.ejs',{})
-})
 
 app.listen('3003',()=>{
     console.log('server is runing at http://127.0.0.1:3003')
